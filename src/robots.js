beba6557438c3e163fb2d070a6da6530cc18648e
Santiago Robayo <s.robayo222@uniandes.edu.co>
2020-02-17 14:30:57 -0500

    initial

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    "id": 1,
+    "name": "Ingrid Long",
+    "username": "Bolton",
+    "email": "boltonlong@terragen.com"
+  },
+  {
+    "id": 2,
+    "name": "Alissa Reeves",
+    "username": "Serena",
+    "email": "serenareeves@terragen.com"
+  },
+  {
+    "id": 3,
+    "name": "Ellis Swanson",
+    "username": "Jacobs",
+    "email": "jacobsswanson@terragen.com"
+  },
+  {
+    "id": 4,
+    "name": "Raquel Booth",
+    "username": "Hensley",
+    "email": "hensleybooth@terragen.com"
+  },
+  {
+    "id": 5,
+    "name": "Ruthie French",
+    "username": "Janette",
+    "email": "janettefrench@terragen.com"
+  },
+  {
+    "id": 6,
+    "name": "Ericka Russo",
+    "username": "Matthews",
+    "email": "matthewsrusso@terragen.com"
+  },
+  {
+    "id": 7,
+    "name": "Donovan Garrett",
+    "username": "Denise",
+    "email": "denisegarrett@terragen.com"
+  },
+  {
+    "id": 8,
+    "name": "Elinor Huber",
+    "username": "Moreno",
+    "email": "morenohuber@terragen.com"
+  },
+  {
+    "id": 9,
+    "name": "Davidson Gardner",
+    "username": "Fern",
+    "email": "ferngardner@terragen.com"
+  },
+  {
+    "id": 10,
+    "name": "Fran Sheppard",
+    "username": "Tessa",
+    "email": "tessasheppard@terragen.com"
+  }
+]
